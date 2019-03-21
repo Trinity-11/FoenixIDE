4ba2a3690e1371e169b16dc5324001232f338383
dtremblayext <daniel.tremblay@mdacorporation.com>
2019-03-21 16:17:14 -0300

    Missed a couple files.

---
+++ FoenixIDE Simulator/ROMs/ReadyHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoenixIDE
+{
+    /// <summary>
+    /// Use this to designate the object that is invoked when the kernel READY method is invoked. 
+    /// This is also the object that is invoked when the Return key on the keyboard is pressed in immediate mode.
+    /// </summary>
+    public interface ReadyHandler
+    {
+        void Ready();
+        void ReturnPressed(int LineStart);
+        void PrintGreeting();
+    }
+}
